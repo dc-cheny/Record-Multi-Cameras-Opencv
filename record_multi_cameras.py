b2491c665a63805db6abd542696db21b053b1eb1
dc-cheny <cheny.sufedc@outlook.com>
2022-03-09 15:31:50 +0800

    record multiple cameras

---
+++ record_multi_cameras.py
@@ -0,0 +1,39 @@
+import threading
+
+import cv2
+
+
+class CamThread(threading.Thread):
+    def __init__(self, previewName, camID):
+        threading.Thread.__init__(self)
+        self.previewName = previewName
+        self.camID = camID
+
+    def run(self):
+        print("Starting " + self.previewName)
+        CamPreview(self.previewName, self.camID)
+
+
+def CamPreview(previewName, camID):
+    cv2.namedWindow(previewName)
+    cam = cv2.VideoCapture(camID)
+    if cam.isOpened():  # try to get the first frame
+        rval, frame = cam.read()
+    else:
+        rval = False
+
+    while rval:
+        cv2.imshow(previewName, frame)
+        rval, frame = cam.read()
+        key = cv2.waitKey(20)
+        if key == 27:  # exit on ESC
+            break
+    cv2.destroyWindow(previewName)
+
+
+if __name__ == '__main__':
+    # Create two threads as follows
+    thread1 = CamThread("Camera 1", 1)
+    thread2 = CamThread("Camera 2", 2)
+    thread1.start()
+    thread2.start()
